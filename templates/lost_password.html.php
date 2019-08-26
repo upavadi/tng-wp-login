006701e586ac3104b4f851fa87ae2581fadd5548
upavadi <mahesh@upavadi.net>
2019-08-26 12:24:43 +0100

    change php comment to html

--- templates/lost_password.html.php
+++ templates/lost_password.html.php
@@ -16,7 +16,7 @@ function lostPassword() {
 	}
 
 	?>
-/** add bootstrap here instead of primary file to avoid conflucts with other plugins **/	
+<!-- add bootstrap here instead of primary file to avoid conflucts with other plugins -->
  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
 <div class="container-fluid">
 	<div id='container_forgot_pw'>
