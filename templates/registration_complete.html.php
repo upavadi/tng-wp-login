e007b1853cf6086a678297495ac8a9ef755f4cd7
upavadi <mahesh@upavadi.net>
2019-10-08 11:29:04 +0100

    correct typo

--- templates/registration_complete.html.php
+++ templates/registration_complete.html.php
@@ -10,7 +10,7 @@ function registration_complete($config) {
 ob_clean();
 ob_start();
 ?>
-/** add bootstrap here instead of primary file to avoid conflucts with other plugins **/	
+<!-- add bootstrap here instead of primary file to avoid conflucts with other plugins -->	
  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
  
 	<div class="container">
