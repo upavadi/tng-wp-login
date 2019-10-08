e00f81121e58351867a27946459cb14399785c20
upavadi <mahesh@upavadi.net>
2019-10-08 11:26:41 +0100

    cporrect typo

--- templates/registration_form.html.php
+++ templates/registration_form.html.php
@@ -1,6 +1,7 @@
 <?php
+
 function input($field, $label, $description, $placeholder, $value, $error, $type = 'text') {
-	$errorClass = '';	
+	$errorClass = '';
 	if ($error) {
 		$errorClass = 'has-error';
 	}
@@ -16,7 +17,7 @@ function input($field, $label, $description, $placeholder, $value, $error, $type
 	}
 ?>
 <!DOCTYPE html>
-<!-- add bootstrap here instead of primary file to avoid conflucts with other plugins -->
+<!-- add bootstrap here instead of primary file to avoid conflicts with other plugins -->
  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
 <html lang="en">
 <script src='https://www.google.com/recaptcha/api.js'></script>	
