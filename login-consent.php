10f53b71c1da68e9c1a145a46245f10d60660c72
upavadi <mahesh@upavadi.net>
2019-11-09 11:30:59 +0000

    update Consent flag

--- login-consent.php
+++ login-consent.php
@@ -6,13 +6,13 @@ require_once "login-to-wp.php";
 require_once "login-to-tng.php";
 //UPDATE IGNORE `tng_users` SET `dt_consented`='0000-00-00 00:00:00' WHERE `username` = 'gondal'
 //add_action( 'wp_login', 'checkConsent' );
- 
 $current_user_consent = newRegPrivacy();
 $current_user_consent_text = $current_user_consent['current_user_consent_text'];
 if(isset($current_user_consent['privacyDoc']))
 $privacy_doc_url = newRegPrivacy()['privacyDoc'];
 
-function checkConsent() {
+function checkConsent() { 
+
   $wpConsent = "";
   $wpCurrentUser = wp_get_current_user() -> user_login;
   $wpUserId = get_current_user_id();
@@ -21,9 +21,7 @@ function checkConsent() {
   $tngRole = roleTng();
   $wpRole = current_user_can('administrator'); 
   $tngVersion = guessTngVersion();
-  $tngTen = false;
-  $tngTwelve = false;
-  $tngConsent = false;
+
   if ($tngVersion < 12) $tngTen = true;
   if ($tngVersion >= 12) $tngTwelve = true;
   
@@ -37,26 +35,30 @@ function checkConsent() {
   {
      $tngConsent = getTngConsent();
   } 
-   
+ 
   //check if consent in both or v10 in use
-  if ($wpConsent > 0 && ($tngConsent > 0 || $tngTen == true ))
+  if ($wpConsent && ($tngConsent > 0 || $tngTen == true ))
   {
     //echo "consented";
     return; //wp and tng consented
   }
-
-  //consent in wp only
-  if ($wpConsent > 0 && ($tngConsent == 0 || $tngTen) ) {
+  
+  //consent in wp and not V12
+  if ($wpConsent > 0 && (isset($tngTen))) return;
+ 
+  if ($wpConsent > 0 && ($tngConsent == 0) ) {
+    
     if($tngTwelve) $success = updateTngConsent();
     return; 
   }
   
   //consent in tng only
-  if (!$wpConsent && ($tngConsent && $tngTwelve )) {
+  if (!$wpConsent && ($tngConsent>0 && $tngTwelve )) {
     //echo "consent in tng only";
     update_user_meta($wpUserId, 'tng_dateconsented', date('Y-m-d h:i:s'));
     return;
   }
+  
 //process consent
   $logoutUrl = (wp_logout_url(home_url()));
   $logoutUrl = str_replace('&amp;', '&' ,$logoutUrl);
@@ -70,12 +72,11 @@ function checkConsent() {
   if (!isset($_GET['value'])) return;
      
    $response =  $_GET['value'];
-  
-   
-     /** user presses OK - udate consent flags in WP and TNG ***/
+    
+   /** user presses OK - udate consent flags in WP and TNG ***/
     if ($response == "true") {
-      
       update_user_meta($wpUserId, 'tng_dateconsented', date('Y-m-d h:i:s'));
+      $success = updateTngConsent();
         return;
     }
       /** user presses cancel - LogOut and return ***/
@@ -83,10 +84,9 @@ function checkConsent() {
         echo '<script>window.location = "'.$logoutUrl.'";</script>'; 
         return;
       }
-       
   return;
-  
 }
+
 ?>
 
 <input type="text" id="alerttext" value="<?php echo $current_user_consent_text; ?>" hidden>
@@ -96,20 +96,17 @@ function checkConsent() {
     var retVal = confirm(consentText);
     if (retVal == true) {
       window.location.href = '/login-consent.php?value=true';
-    
     } else {
       window.location.href = '/login-consent.php?value=false';
     }
     return;   
-
 }
 </script>
 
 <?php
 //UPDATE IGNORE `tng_users` SET `dt_consented`='0000-00-00 00:00:00' WHERE `username` = 'gondal'
 function getTngConsent() {
-  
-	$tng_path = getSubroot(). "config.php";
+  $tng_path = getSubroot(). "config.php";
 	include ($tng_path); 
 	$db = mysqli_connect($database_host, $database_username, $database_password, $database_name);
 	if ($db->connect_error) {
@@ -133,8 +130,8 @@ function updateTngConsent() {
   $wpCurrentUser = wp_get_current_user() -> user_login;
   $dt_consented = date('Y-m-d h:i:s');
   $db = mysqli_connect($database_host, $database_username, $database_password, $database_name);
-
-  $sql = "UPDATE tng_users SET dt_consented='$dt_consented' WHERE username='$wpCurrentUser' ";
-  $success = mysqli_query($db, $sql);
-  return $success;
+  $stmt = $db->prepare("UPDATE tng_users SET dt_consented= ? WHERE username = ?");
+$stmt->bind_param("ss", $dt_consented, $wpCurrentUser);
+$success = $stmt->execute();
+$stmt->close();
 }
